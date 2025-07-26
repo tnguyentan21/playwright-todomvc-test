6591e4d421add7646a2fc18d864490e27bcd1205
tnguyentan21 <tnguyentan21@gmail.com>
2025-07-26 23:14:34 +0700

    add test for Learn Playwright

--- tests/add-todo.spec.js
+++ tests/add-todo.spec.js
@@ -7,4 +7,7 @@ test('should add a new todo item', async ({ page }) => {
   await todo.addTodo('Learn POM');
   const todos = await todo.getTodos();
   expect(todos).toContain('Learn POM');
+  await todo.addTodo('Learn Playwright');
+  const todos2 = await todo.getTodos();
+  expect(todos2).toContain('Learn Playwright');
 });
